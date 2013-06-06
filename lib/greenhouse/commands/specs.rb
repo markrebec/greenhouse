0ff317a01ae1b4bd4a577768f4caa32c104a665f
Mark Rebec <mark@markrebec.com>
2013-06-06 14:45:36 -0700

    stripping out forthrail specific specs, replacing placeholder specs command

--- lib/greenhouse/commands/specs.rb
+++ lib/greenhouse/commands/specs.rb
@@ -2,47 +2,10 @@ module Greenhouse
   module Commands
     class Specs
       include Command
-      command_summary "Run rspec for one or all of your projects"
-      # TODO -q/--quiet argument
-      valid_argument Scripts::Argument.new("-q, --quiet", [], :summary => "Do not print out detailed information about specs and coverage")
-      project_arguments *Projects::projects.map(&:to_arg)
+      command_summary "Not Yet Implemented"
       
-      class << self
-        def usage
-          puts <<USAGE
-usage: #{::Greenhouse::CLI.command_name} #{command_name} <#{valid_arguments.map(&:key).join(", ")}> [<project>]
-
-Arguments:
-#{valid_arguments.to_help}
-
-Projects:
-#{project_arguments.to_help}
-USAGE
-        end
-      end
-
-      def quiet?
-        arguments.map(&:key).include?("-q")
-      end
-
-      def run_all_specs
-        results = Resources::Specs::Results.new
-        Projects::projects.each do |project|
-          results << Tasks::ProjectSpecs.perform(project).results
-        end
-        puts
-        results.output(!quiet?)
-      end
-
-      def run_project_specs
-        results = Resources::Specs::Results.new([Tasks::ProjectSpecs.perform(@project).results])
-        puts
-        results.output(!quiet?)
-      end
-
       def run
-        @project = Projects::projects.select { |project| arguments.map(&:key).include?(project.name) }.first
-        @project.nil? ? run_all_specs : run_project_specs
+        puts "Not Yet Implemented"
       end
 
     end
